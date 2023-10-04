2d341328cb7eebc0948663238447a54f00823e0f
Eidcarosse <eidcarosse@gmail.com>
2023-10-04 12:48:01 +0500

    update

--- src/redux/slices/config/index.js
+++ src/redux/slices/config/index.js
@@ -1,6 +1,20 @@
-import configSlice from './slice';
+import configSlice from "./slice";
 export const configSliceReducer = configSlice.reducer;
-export const {setAppLoader,setTopAds} = configSlice.actions;
+export const {
+  setAppLoader,
+  setTopAds,
+  setBrandFilter,
+  setCategoryFilter,
+  setFilter,
+  setMaxPriceFilter,
+  setMinPriceFilter,
+  setModelFilter,
+} = configSlice.actions;
 export const selectLoader = (state) => state.config.appLoader;
 export const selectTopAds = (state) => state.config.topads;
-export const selectTopAds = (state) => state.config.topads;
+export const selectFilter = (state) => state.config.filter;
+export const selectBrandFilter = (state) => state.config.filter?.brand;
+export const selectCategoryFilter = (state) => state.config.filter?.category;
+export const selectMaxPriceFilter = (state) => state.config.filter?.maxPrice;
+export const selectMinPriceFilter = (state) => state.config.filter?.minPrice;
+export const selectModelFilter = (state) => state.config.filter?.model;
