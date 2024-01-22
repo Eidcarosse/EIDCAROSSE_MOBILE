f43839b5b136b0829be57e680f5ac67947ffc5b3
Eidcarosse <eidcarosse@gmail.com>
2024-01-22 17:55:29 +0500

    search history

--- src/components/searchbar/index.js
+++ src/components/searchbar/index.js
@@ -1,7 +1,13 @@
-import { Feather, AntDesign, Ionicons } from "@expo/vector-icons";
+import { Feather, AntDesign,Entypo, Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
-import { TextInput, TouchableOpacity, View } from "react-native";
+import React, { Fragment, useState, useEffect } from "react";
+import {
+  TextInput,
+  TouchableOpacity,
+  View,
+  Text,
+  Platform,
+} from "react-native";
 import { useDispatch } from "react-redux";
 import ScreenNames from "../../routes/routes";
 import AppColors from "../../utills/AppColors";
@@ -9,7 +15,74 @@ import { height, width } from "../../utills/Dimension";
 import Button from "../button";
 import styles from "./styles";
 import { useTranslation } from "react-i18next";
-
+import { FlatList } from "react-native-gesture-handler";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+var items = [
+  {
+    id: 1,
+    name: "JavaScript",
+  },
+  {
+    id: 2,
+    name: "Java",
+  },
+  {
+    id: 3,
+    name: "Ruby",
+  },
+  {
+    id: 4,
+    name: "React Native",
+  },
+  {
+    id: 5,
+    name: "PHP",
+  },
+  {
+    id: 6,
+    name: "Python",
+  },
+  {
+    id: 7,
+    name: "Go",
+  },
+  {
+    id: 8,
+    name: "Swift",
+  },
+  {
+    id: 1,
+    name: "JavaScript",
+  },
+  {
+    id: 2,
+    name: "Java",
+  },
+  {
+    id: 3,
+    name: "Ruby",
+  },
+  {
+    id: 4,
+    name: "React Native",
+  },
+  {
+    id: 5,
+    name: "PHP",
+  },
+  {
+    id: 6,
+    name: "Python",
+  },
+  {
+    id: 7,
+    name: "Go",
+  },
+  {
+    id: 8,
+    name: "Swift",
+  },
+];
 export default function SearchBar({
   search,
   setSearch,
@@ -20,84 +93,167 @@ export default function SearchBar({
   const { t } = useTranslation();
   const navigation = useNavigation();
   const dispatch = useDispatch();
-  const handleInputSubmit = () => {
-    // Navigate to the next screen here
+  const [isFocused, setIsFocused] = useState(false);
+  const [searchHistory, setSearchHistory] = useState([]);
 
+  useEffect(() => {
+    // Load the search history from AsyncStorage when the component mounts
+    loadSearchHistory();
+  }, []);
+
+  useEffect(() => {
+    // Save the current search to the search history whenever it changes
+    saveSearchHistory();
+  }, [searchHistory]);
+
+  const saveSearchHistory = async () => {
+    try {
+      await AsyncStorage.setItem(
+        "searchHistory",
+        JSON.stringify(searchHistory)
+      );
+    } catch (error) {
+      console.error("Error saving search history:", error);
+    }
+  };
+
+  const loadSearchHistory = async () => {
+    try {
+      const savedSearchHistory = await AsyncStorage.getItem("searchHistory");
+      if (savedSearchHistory !== null) {
+        setSearchHistory(JSON.parse(savedSearchHistory));
+      }
+    } catch (error) {
+      console.error("Error loading search history:", error);
+    }
+  };
+
+  const handleSearch = () => {
+    const newSearchHistory = [...searchHistory, search];
+    setSearchHistory(newSearchHistory);
+  };
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setTimeout(() => {
+      setIsFocused(false);
+    }, 2000);
+  };
+  const handleDelete = (index) => {
+    // Delete the search at the specified index
+    const newSearchHistory = [...searchHistory];
+    newSearchHistory.splice(index, 1);
+    setSearchHistory(newSearchHistory);
+  };
+  const handleInputSubmit = () => {
+    let a = searchHistory.find((item) => item == search);
+    // Navigate to the next screen here
+    if (!a) handleSearch();
     next
       ? (navigation.navigate(ScreenNames.LISTDATA, { search: search }),
         setSearch(""))
       : onPress();
   };
+
+  const renderItem = ({ item, index }) => {
+    if (item.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+      return (
+        <View style={styles.flatcontainer}>
+          <Entypo name="back-in-time" size={height(2)} color={"grey"} />
+          <TouchableOpacity
+            style={{ width: width(67) }}
+            onPress={() => {
+              setSearch(item);
+            }}
+          >
+            <Text>{item}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{ marginLeft: height(2), padding: height(0.5) }}
+            onPress={() => {
+              handleDelete(index);
+            }}
+          >
+            <AntDesign name="close" size={height(2.5)} color={"grey"} />
+          </TouchableOpacity>
+        </View>
+      );
+  };
   return (
-    <View style={styles.container}>
-      <View
-        style={[
-          search ? styles.main : containerstyle,
-          { borderColor: search ? AppColors.primary : "black" },
-        ]}
-      >
-        <Ionicons
-          name="search"
-          style={{ marginHorizontal: height(1) }}
-          color={search ? AppColors.primary : "grey"}
-          size={height(2.5)}
-        />
-        <TextInput
-         blurOnSubmit={true}
-        autoCapitalize='none'
-          placeholder={t("searchbar.phsearch")}
-          value={search}
-          onChangeText={setSearch}
-          style={{ width: width(55), fontSize: height(1.5) }}
-          onSubmitEditing={handleInputSubmit}
-        />
-        {search && (
-          <Button
-            onPress={handleInputSubmit}
-            title={"searchbar.search"}
-            containerStyle={{
-              flex: 1,
-              padding: height(0.1),
-              paddingVertical: height(0.2),
-              backgroundColor: AppColors.primary,
-              borderRadius: height(.2),
-              marginVertical: height(0.2),
-            }}
-            textStyle={{
-              fontSize: height(1.5),
-              margin: height(0.1),
-              padding: height(0.1),
-            }}
+    <Fragment>
+      <View style={styles.container}>
+        <View
+          style={[
+            search ? styles.main : containerstyle,
+            { borderColor: search ? AppColors.primary : "black" },
+          ]}
+        >
+          <Ionicons
+            name="search"
+            style={{ marginHorizontal: height(1) }}
+            color={search ? AppColors.primary : "grey"}
+            size={height(2.5)}
           />
-        )}
-      </View>
-      {next && search == "" ? (
-        <View>
-          {/* <TouchableOpacity
+          <TextInput
+            blurOnSubmit={true}
+            autoCapitalize="none"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            placeholder={t("searchbar.phsearch")}
+            value={search}
+            onChangeText={setSearch}
+            style={{ width: width(55), fontSize: height(1.5) }}
+            onSubmitEditing={handleInputSubmit}
+          />
+          {search && (
+            <Button
+              onPress={handleInputSubmit}
+              title={"searchbar.search"}
+              containerStyle={styles.serachBtn}
+              textStyle={styles.searchTxt}
+            />
+          )}
+        </View>
+        {next && search == "" ? (
+          <View>
+            {/* <TouchableOpacity
             style={{ marginLeft: height(2) }}
             onPress={() => navigation.navigate(ScreenNames.MAP)}
           >
             <Feather name="globe" size={height(3.5)} color={AppColors.primary} />
           </TouchableOpacity> */}
-        </View>
-      ) : (
-        search != "" && (
-          <View>
-            <TouchableOpacity
-              style={{ marginLeft: height(2) }}
-              onPress={() => {
-                setSearch(""), !next && onPress();
-              }}
-            >
-              <AntDesign
-                name="closesquare"
-                size={height(3.5)}
-                color={AppColors.primary}
-              />
-            </TouchableOpacity>
           </View>
-        )
+        ) : (
+          search != "" && (
+            <View>
+              <TouchableOpacity
+                style={{ marginLeft: height(2) }}
+                onPress={() => {
+                  setSearch(""), !next && onPress();
+                }}
+              >
+                <AntDesign
+                  name="closesquare"
+                  size={height(3.5)}
+                  color={AppColors.primary}
+                />
+              </TouchableOpacity>
+            </View>
+          )
+        )}
+      </View>
+      {isFocused && (
+        <View style={styles.flatView}>
+          <FlatList
+            data={searchHistory}
+            renderItem={renderItem}
+            keyExtractor={(item, index) => index}
+          />
+        </View>
       )}
-    </View>
+    </Fragment>
   );
 }
--- src/components/searchbar/styles.js
+++ src/components/searchbar/styles.js
@@ -11,10 +11,46 @@ const styles = StyleSheet.create({
   main: {
     width: width(85),
     borderRadius: height(1),
-    padding: height(.5),
+    padding: height(0.5),
     flexDirection: "row",
     borderWidth: height(0.05),
     alignItems: "center",
   },
+  serachBtn: {
+    flex: 1,
+    padding: height(0.1),
+    paddingVertical: height(0.2),
+    backgroundColor: AppColors.primary,
+    borderRadius: height(0.2),
+    marginVertical: height(0.2),
+  },
+  searchTxt:{
+    fontSize: height(1.5),
+    margin: height(0.1),
+    padding: height(0.1),
+  },
+  flatcontainer: {
+    padding: height(0.5),
+    margin: height(0.5),
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    elevation: 1,
+    backgroundColor: "white",
+    borderRadius: height(1),
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.1,
+    shadowRadius: 1,
+  },
+  flatView:{
+    backgroundColor: "white",
+    maxHeight: height(30),
+    width: width(95),
+    position: "absolute",
+    zIndex: 1,
+    top: height(7),
+    flexDirection: "row",
+  }
 });
 export default styles;
